038caefaef938e1562e90d74a9c835503c8ff6d2
Murad Najafli <muradnecefli05@gmail.com>
2023-06-10 17:45:54 +0400

    SuffixingApp

--- src/main/java/ConfigFileReader.java
+++ src/main/java/ConfigFileReader.java
@@ -1,10 +1,6 @@
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class ConfigFileReader {
     public Properties readConfigFile(String filePath) throws IOException {
